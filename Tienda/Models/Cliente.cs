0562e337643a5f11e0078bf4f1d60e34b3b4e73c
Erick Noriega <noriegaerick83@gmail.com>
2026-08-13 22:50:22 -0300

    Guardando las migraciones de EF

--- Tienda/Models/Cliente.cs
+++ Tienda/Models/Cliente.cs
@@ -14,8 +14,11 @@ namespace Tienda.Models
         public string Apellido { get; set; }
         public string? ImagenurlPerfil { get; set; }
         public DateTime FechaNac { get; set; }
-
+        [Required]
+        [StringLength(50)]
         public string Correo { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Contraseña { get; set; }
         public List<Pedido>? pedidos { get; set; }
         public List<Producto>? productos { get; set; }
